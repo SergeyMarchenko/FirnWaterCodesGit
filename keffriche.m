d57302fcb026d5983ea0da9228b59e370edef936
SergeyMarchenko <62382486+SergeyMarchenko@users.noreply.github.com>
2021-01-18 13:51:50 +0300

    Delete keffriche.m

--- keffriche.m
+++
@@ -1,9 +0,0 @@
-function [ k ] = keffriche( r )
-%keffrische( r ) returns the effective thermal conductivity of snow/firn (k) in [J (s m K)^-1] 
-% as a function of density (r) in [kg m^-3] following:
-% Riche, F. and Schneebeli, M.: Thermal conductivity of snow measured
-% by three independent methods and anisotropy considerations,
-% The Cryosphere, 7, 217–227, https://doi.org/10.5194/tc-7-
-% 217-2013, 2013.
-k   = 3   * 10^-6 * r.^2 - 1.06 .* 10^-5 .* r + 0.024;
-end
